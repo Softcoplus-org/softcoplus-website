5b952042622d4070e78c6351db9f8d2b11972ea8
shubhamsingh2468 <98507450+shubhamsingh2468@users.noreply.github.com>
2022-03-01 13:31:50 +0530

    Updated footer.tsx files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,24 @@ import logo from  "../public/logo.jpeg"
 
 const navigation = {
   navlinks: [
-    { name:'solutions', href: 'solutions' },
+    { name:'Solutions', href: 'solutions' },
+    { name: 'Services', href: '/services' },
+    { name: 'Home', href: '/' },
+    { name: 'AboutUs', href: '/about' },
+    { name: 'Contact', href: '/contact' },
 
   ],
   services: [
-    { name: 'services', href: '/services' },
+    { name: 'Services', href: '/services' },
   ],
   Home: [
-    { name: 'home', href: '/' },
+    { name: 'Home', href: '/' },
   ],
   contactus: [
-    { name: 'contact', href: '/contact' },
-  ],
-  employee: [
-    { name: 'employee', href: '/employee' },
+    { name: 'Contact', href: '/contact' },
   ],
   AboutUs: [
-    { name: 'about', href: '/AboutUs' },
+    { name: 'AboutUs', href: '/about' },
   ],
 
 
@@ -32,7 +33,7 @@ const navigation = {
 
 export default function Example() {
     return (
-    <footer className="bg-white" aria-labelledby="footer-heading">
+    <footer className="bg-black" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
@@ -49,14 +50,15 @@ export default function Example() {
             </div>
             </div>
             </div>
-            <p className="text-gray-500 text-right bg-red-300">
+            <p className="text-white-500 text-right bg-red-600">
                 Making the world a better place through constructing elegant hierarchies.
               </p>
-              <div className="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
-              <div className="md:grid md:grid-cols-2 md:gap-8">
+              <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase text-center mt-10">Quick Links</h3>
+              <div className="mt-12 grid grid-flow-row gap-8 xl:mt-0 xl:rows-span-2 pt-20">
+              <div className="md:grid md:grid-flow-row md:gap-8">
                 <div>
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-4">
+                  
+                  <ul role="list" className="ml-10 space-x-4 m-8 grid grid-flow-col gap-1 ">
                     {navigation.navlinks.map((item) => (
                       <li key={item.name}>
                         <a href={item.href} className="text-base text-gray-500 hover:text-red-900">
@@ -67,72 +69,29 @@ export default function Example() {
                   </ul>
                 </div>
 
+    </div>
               </div>
-              <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-4">
-                    {navigation.services.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-4">
-                    {navigation.Home.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-4">
-                    {navigation.contactus.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-4">
-                    {navigation.employee.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-12 md:mt-0">
-                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"></h3>
-                  <ul role="list" className="ml-10 space-y-10">
-                    {navigation.AboutUs.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+          
+
+
               
-              
-            <div className="mt-12 border-t border-gray-200 pt-8 mb-8">
+          
+          <div className="mt-12 border-t border-gray-200 pt-8 mb-8">
             <p className="text-base text-gray-400 xl:text-center">&copy; 2020 Softcoplus,  Inc. All rights reserved.</p>
           </div>
           </footer>
             )}
+
+
+
+
+  
+    
+
+ 
+
+
+          
+             
+                
+     
